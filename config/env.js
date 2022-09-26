7a91180080a4ca96122aa792a3b273a9cdf42127
mrizwanashiq <mrizwanashiq@outlook.com>
2022-09-27 00:49:41 +0500

    Fixed few things

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 const environment = {
   port: process.env.PORT || 2022,
   nodeEnv: process.env.NODE_ENV || "development",
-  mongodbUri: process.env.DB_URI || "mongodb://localhost:27017/shopalaDb",
+  mongodbUri: process.env.DB_URI || "mongodb://localhost:27017/boilerplate",
 };
 
 export default environment;
