917ab064b670791c9af0903fec885b3debb9e3f9
mrizwanashiq <mrizwanashiq@outlook.com>
2023-04-22 03:01:56 +0500

    feat

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 export const env = {
 	port: process.env.PORT || 2022,
 	nodeEnv: process.env.NODE_ENV || "development",
-	mongodbUri: process.env.DB_URI || "mongodb://localhost:27017/boilerplate",
+	mongodbUri: process.env.DB_URI || "mongodb://127.0.0.1:27017/boilerplate",
 	jwtSecret: process.env.JWT_SECRET || "my_temporary_secret",
 };
