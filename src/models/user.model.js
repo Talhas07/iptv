7ce6eba28e5ff836e2dcc68f134f9817c63c5e71
mrizwanashiq <mrizwanashiq@outlook.com>
2023-04-26 00:27:47 +0500

    feat

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
-const schema = mongoose.Schema({
-	name: { type: String, required: true, maxlength: 50 },
-});
+const schema = mongoose.Schema(
+	{
+		name: { type: String, required: true, maxlength: 50 },
+	},
+	{ timestamps: true }
+);
 export const UserModel = mongoose.model("User", schema);
